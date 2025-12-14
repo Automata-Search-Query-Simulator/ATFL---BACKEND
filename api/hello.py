5c0f23f9ee0c101b66f66a36f59409e29019a988
Rhenel Jhon Sajol <sajol.rhenel123@gmail.com>
2025-12-14 11:24:01 +0800

    Remove vercel.json to use auto-detection

--- api/hello.py
+++ api/hello.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/api/hello')
 def hello():
-    return {"message": "Hello from Vercel!", "status": "ok"}
+    return jsonify({"message": "Hello from Vercel!", "status": "ok"})
 
